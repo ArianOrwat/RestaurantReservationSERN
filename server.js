d551e5c403193d2e73db76e35b614fee6e1df7e5
Arian Orwat <arian.orwat@gmail.com>
2020-02-03 05:16:02 +0100

    user register

--- server.js
+++ server.js
@@ -1,41 +1,17 @@
 const express = require('express');
-const db = require('./config/db');
+const db = require("./config/db");
+const mysql = require('mysql');
 
+db.connect();
 const app = express();
 
-// Connect to database
-db.connect();
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API RUNNING'));
 
-app.get('/adduser', async (req, res) => {
-    const user = {
-        username: 'Arian',
-        email: 'arian.orwat@gmail.com',
-        password: 'pass',
-        image: './userImage/1',
-        created: new Date(),
-        passwordDate: new Date()
-    };
-
-    await db.query('INSERT INTO users SET ?', user);
-    res.send("User created"); 
-});
-
-app.get('/checkdate/:id', (req, res) => {
-    const sql = `SELECT * FROM users WHERE id = ${req.params.id}`
-    db.query(sql, (err, result) => {
-        if (err) res.send("DATABASE ERROR");
-        else if(result.length === 0) res.send("ERROR USER NOT FIND");
-        else {
-            const user = result[0];
-            if(new Date() > user.created) console.log("Yeeey");
-            res.send(user);
-        }
-    });
-})
-
-
+// Define Routes
+app.use('/api/users', require('./routes/users'));
 
 const PORT = process.env.PORT || 5000;
 
