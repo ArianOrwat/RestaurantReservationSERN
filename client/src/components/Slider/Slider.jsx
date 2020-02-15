ec7798da0e18f46a1eb1ba8d68dfe5f351371bdf
Arian Orwat <arian.orwat@gmail.com>
2020-02-16 00:58:49 +0100

    Dots on slider start

--- client/src/components/Slider/Slider.jsx
+++ client/src/components/Slider/Slider.jsx
@@ -8,25 +8,56 @@ class Slider extends React.Component {
     this.state = {
       imgNum: 0,
       image: this.props.images[0],
-      arrow: ["<", ">"]
+      arrow: ["<", ">"],
+      dots: []
     };
   }
 
+  componentDidMount() {
+      this.defaultDots();
+  }
+
+  defaultDots = () => {
+    const imagesArrayLength = this.props.images.length - 1;
+    const dotsArrayBuffer = [];
+    for (let i = 0; i < imagesArrayLength; i++) {
+      dotsArrayBuffer.push(0);
+    }
+    this.setState({ dots: [1, ...dotsArrayBuffer] });
+  };
+
   changeToNext = () => {
-    console.log("next");
-    if (this.props.images.length - 1 === this.state.imgNum ) {
-      this.setState({imgNum: 0, image: this.props.images[0]});
+    //console.log("next");
+    if (this.props.images.length - 1 === this.state.imgNum) {
+      this.setState({ imgNum: 0, image: this.props.images[0] });
+      this.defaultDots();
     } else {
-        this.setState(function (prevState) { return { imgNum: prevState.imgNum + 1, image: this.props.images[prevState.imgNum + 1]}});
+      const dot = this.state.dots.find(dot => dot === 1);
+      console.log(dot)
+      this.setState(function(prevState) {
+        return {
+          imgNum: prevState.imgNum + 1,
+          image: this.props.images[prevState.imgNum + 1],
+        };
+      });
     }
   };
 
   changeToPrev = () => {
-    console.log("prev");
+    //console.log("prev");
     if (this.state.imgNum === 0) {
-        this.setState({ imgNum: this.props.images.length - 1, image: this.props.images[this.props.images.length - 1]});
+      this.setState({
+        imgNum: this.props.images.length - 1,
+        image: this.props.images[this.props.images.length - 1]
+      });
+      this.defaultDots();
     } else {
-        this.setState(function (prevState) { return { imgNum: prevState.imgNum - 1, image: this.props.images[prevState.imgNum - 1]}});
+      this.setState(function(prevState) {
+        return {
+          imgNum: prevState.imgNum - 1,
+          image: this.props.images[prevState.imgNum - 1]
+        };
+      });
     }
   };
   render() {
@@ -40,7 +71,21 @@ class Slider extends React.Component {
           <div className="rightArrow" onClick={() => this.changeToNext()}>
             {this.state.arrow[1]}
           </div>
-          <img src={this.state.image} alt={`img`} key={`img`} className="slider__img" />
+          <div className="dots">
+            {this.state.dots.map((dot, id) =>
+              dot ? (
+                <div className="dot active" key={`dot${id}`}></div>
+              ) : (
+                <div className="dot" key={`dot${id}`}></div>
+              )
+            )}
+          </div>
+          <img
+            src={this.state.image}
+            alt={`img`}
+            key={`img`}
+            className="slider__img"
+          />
         </div>
       </>
     );
